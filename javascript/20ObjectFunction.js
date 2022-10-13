ed3a9cb7bbfbfd10d81791c9c9ead833d4d58969
Vashu Haritasya <vashusharma246@gmail.com>
2022-10-13 23:05:32 +0530

    Classes

--- javascript/20ObjectFunction.js
+++ javascript/20ObjectFunction.js
@@ -11,3 +11,14 @@ calc.sum(2, 5);
 7;
 calc.sub(9, 7);
 2;
+
+////////////////////////
+function language(name, country) {
+  this.name = name;
+  this.country = country;
+  this.greet = () => {
+    return `Say Hi to ${name}`;
+  };
+}
+
+var Hindi = new language("Hindi", "india");
