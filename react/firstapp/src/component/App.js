35b0defb555a501fe1c8e11ae0841bbfea119f77
Vashu Haritasya <vashusharma246@gmail.com>
2022-11-26 19:40:12 +0530

    React

---
+++ react/firstapp/src/component/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Heya from React</h1>
+      <h2> with react</h2>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
